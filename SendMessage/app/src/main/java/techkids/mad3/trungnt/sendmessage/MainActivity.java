af60d6dcbee629a29984e2a4ba24d83ce14d2ae6
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-02 20:50:54 +0700

    define typeInput method (temp)

--- SendMessage/app/src/main/java/techkids/mad3/trungnt/sendmessage/MainActivity.java
+++ SendMessage/app/src/main/java/techkids/mad3/trungnt/sendmessage/MainActivity.java
@@ -1,13 +1,30 @@
 package techkids.mad3.trungnt.sendmessage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        typeInput();
+    }
+
+    //phuong thuc nhap du lieu tu EditText
+    private void typeInput()
+    {
+        //String inputData;
+        //inputData = this.findViewById(R.id.editInput).toString();
+
+        Intent intentData = new Intent(this, DisplayActivity.class);
+        startActivity(intentData);
     }
 }
