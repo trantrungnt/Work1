d33fdac8c27ca2e8be9f904f4e0e75c0f07b796b
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-02 20:31:37 +0700

    add DisplayActivity class (temp)

---
+++ SendMessage/app/src/main/java/techkids/mad3/trungnt/sendmessage/DisplayActivity.java
@@ -0,0 +1,15 @@
+package techkids.mad3.trungnt.sendmessage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TrungNT on 4/2/2016.
+ */
+public class DisplayActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+    }
+}
