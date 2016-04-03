5c2cb2ddbab1562f899653524f8b8fa390b35ec8
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-03 08:14:33 +0700

    alert dialog when user can't type a value in edittext

--- SendMessage/app/src/main/java/techkids/mad3/trungnt/sendmessage/MainActivity.java
+++ SendMessage/app/src/main/java/techkids/mad3/trungnt/sendmessage/MainActivity.java
@@ -1,5 +1,8 @@
 package techkids.mad3.trungnt.sendmessage;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,7 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    //final Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +31,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = v.getId();
         if(id == R.id.btnSend)
         {
-
             String inputData;
             inputData = ((EditText) findViewById(R.id.editInput)).getText().toString();
-            Bundle bundle = new Bundle();
-            bundle.putString("KeyData", inputData);
+            if (inputData.isEmpty() || inputData == null)
+            {
+                Context context = this;
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                // set title
+                alertDialogBuilder.setTitle("WARNING ...");
 
-            Intent intentData = new Intent(this, DisplayActivity.class);
-            intentData.putExtras(bundle);
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("You need to type your data!")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
 
-            startActivity(intentData);
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+            }
+            else {
+                Bundle bundle = new Bundle();
+                bundle.putString("KeyData", inputData);
+
+                Intent intentData = new Intent(MainActivity.this, DisplayActivity.class);
+                intentData.putExtras(bundle);
+
+                startActivity(intentData);
+            }
         }
     }
 }
